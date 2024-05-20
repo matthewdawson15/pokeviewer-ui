8c4972692482e9ada93cc43b1db1d989df5982f0
Matthew Dawson <60929307+matthewdawson15@users.noreply.github.com>
2024-05-20 23:22:50 +0100

    added pokemon types

--- src/types/pokemonApi.ts
+++ src/types/pokemonApi.ts
@@ -7,6 +7,78 @@ export type NamedAPIResource = {
   url: string;
 };
 
+type SpritesFront = {
+  front_default: string;
+  front_female: string | null;
+  front_shiny: string | null;
+  front_shiny_female: string | null;
+};
+
+interface SpritesDefault extends SpritesFront {
+  back_default: string;
+  back_female: string | null;
+  back_shiny: string | null;
+  back_shiny_female: string | null;
+}
+
+type SpritesOther = {
+  dream_world: {
+    front_default: string;
+    front_female: string | null;
+  };
+  home: SpritesFront;
+  "official-artwork": {
+    front_default: string;
+    front_shiny: string | null;
+  };
+  showdown: SpritesDefault;
+};
+
+/**
+ * "versions" is not fully typed due to time constraints and it not being used by
+ * the PokeViewer but would be in production
+ */
+interface Sprites extends SpritesDefault {
+  other: SpritesOther;
+  versions: Object;
+}
+
+export type Type = {
+  slot: number;
+  type: NamedAPIResource;
+};
+
+export type Cries = {
+  latest: string;
+  legacy: string;
+};
+
+type Stat = {
+  base_stat: number;
+  effort: number;
+  stat: NamedAPIResource;
+};
+
+type VersionGroupDetail = {
+  level_learned_at: number;
+  version_group: NamedAPIResource;
+  move_learn_method: NamedAPIResource;
+};
+
+/**
+ * version_group_details is not used by PokeViewer but would be fully typed in production version
+ */
+export type Move = {
+  move: NamedAPIResource;
+  version_group_details: VersionGroupDetail[];
+};
+
+export type Ability = {
+  ability: NamedAPIResource;
+  is_hidden: boolean;
+  slot: number;
+};
+
 /**
  * PokeAPI response object
  */
@@ -17,6 +89,30 @@ export type PokeApiRes = {
   results: NamedAPIResource[];
 };
 
+/**
+ * Properties not accessed by the PokeViewer are not fully typed for this demo,
+ * but would be fully typed in a production version without time constraints
+ */
+export type Pokemon = {
+  abilities: Ability[];
+  base_experience: number;
+  cries: Cries;
+  forms: NamedAPIResource[];
+  game_indices: any[];
+  height: number;
+  held_items: [];
+  id: number;
+  is_default: boolean;
+  location_area_encounters: string;
+  moves: Move[];
+  name: string;
+  order: number;
+  species: NamedAPIResource;
+  sprites: Sprites;
+  stats: Stat[];
+  types: Type[];
+  weight: number;
+};
 
 export type PokeAPIParams = {
   offset: number;
--- src/types/pokemonDTO.ts
+++ src/types/pokemonDTO.ts
@@ -1,3 +1,48 @@
+import { NamedAPIResource } from "./pokemonApi";
+
+export type AbilityDTO = {
+  ability: string;
+  isHidden: boolean;
+};
+
+export type MoveDTO = {
+  move: string;
+  levelLearned: number;
+};
+
+export type CharacteristicsDTO = {
+  abilities: AbilityDTO[];
+  moves: MoveDTO[];
+  types: string[];
+};
+
+export type ImagesDTO = {
+  artwork: string;
+  animation: string;
+};
+
+export type MediaDTO = {
+  images: ImagesDTO;
+  cry: string;
+};
+
+/**
+ * Data Transfer Object containing all PokeAPI data used by the
+ * PokeViewer, manipulated into the correct format to be visualised.
+ *
+ * All untyped and unused PokeAPI properties are excluded from this
+ * DTO to prevent any unintended usage.
+ */
+export type PokemonDTO = {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  species: string;
+  characteristics: CharacteristicsDTO;
+  media: MediaDTO;
+};
+
 /**
  * Initial properties for Pokemon Tile, consisting of name,
  * url for further info, and the pokemon's unique ID
