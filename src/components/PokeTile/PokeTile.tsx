14b499e7ead061005acf4d945c6345364af1c64c
Matthew Dawson <60929307+matthewdawson15@users.noreply.github.com>
2024-05-20 20:34:53 +0100

    moved primitive helper functions inside PokeTile to reduce scope

--- src/components/PokeTile/PokeTile.tsx
+++ src/components/PokeTile/PokeTile.tsx
@@ -18,6 +18,70 @@ type PokeTileProps = {
 function PokeTile({ onClick, pokeTileDTO }: PokeTileProps): ReactElement {
   const [imageLoaded, setImageLoaded] = useState<boolean>(false);
 
+  /**
+   * Function to generate the image URL for each pokemon tile
+   *
+   * (pokeImageBaseUrl would be moves to a constant file if used in
+   * multiple locations)
+   *
+   * @param id the pokemon's unique ID
+   * @returns raw github URL string linking to the official artwork
+   * image of the pokemon on the PokeAPI's repo
+   */
+  function createPokeImageURL(id: number): string {
+    const pokeImageBaseUrl: string =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+    return pokeImageBaseUrl + id + ".png";
+  }
+
+  /**
+   * Function to parse the Pokemon's unique ID from it's URL
+   *
+   * @param url the Pokemon's API url
+   * @returns the pokemon's unique ID
+   */
+  function parsePokeID(url: string): number {
+    const id: string | undefined = url
+      .split("/")
+      .filter((string: string) => string)
+      .pop();
+
+    if (id) {
+      return parseInt(id);
+    } else {
+      throw new Error("Cannot parse Pokemon ID from provided URL");
+    }
+  }
+
+  /**
+   * Function to capitalise a string
+   *
+   * @param string the string to capitalise
+   * @returns the capitalised string
+   */
+  function capitalise(string: string): string {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  /**
+   * Function to create a human readable version of
+   * a pokemon's name, including capitalisation and a more
+   * user friendly male/female designation
+   *
+   * @param name the original pokemon name string
+   * @returns the more user friendly name
+   */
+  function generatePokeName(name: string): string {
+    const userFriendlyName: string = name.endsWith("-m")
+      ? name.slice(0, -2) + " (Male)"
+      : name.endsWith("-f")
+      ? name.slice(0, -2) + " (Female)"
+      : name;
+
+    return capitalise(userFriendlyName);
+  }
+
   return (
     <button className="poke-tile" onClick={onClick}>
       {!imageLoaded && (
