5aa725c403ab2f271d9d67bd93f3226890832d42
Matthew Dawson <60929307+matthewdawson15@users.noreply.github.com>
2024-05-21 21:58:33 +0100

    move audio play to pokemon image instead of play button

--- src/components/PokeDetails/PokeDetails.tsx
+++ src/components/PokeDetails/PokeDetails.tsx
@@ -2,8 +2,6 @@ import React, { ReactElement, useState } from "react";
 import Button from "../../blocks/Button/Button";
 import { generatePokeName } from "../../helpers/string";
 import { PokemonDTO } from "../../types/pokemonDTO";
-import Icon from "../../blocks/Icon/Icon";
-import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
 import "./PokeDetails.scss";
 
 type PokeDetailsProps = {
@@ -34,24 +32,18 @@ function PokeDetails({
 
   return (
     <div className="poke-details">
-      <img
-        className="poke-details__image"
-        src={pokeDetails.media.artwork}
-        alt={`official artwork for ${pokeDetails.name}(#${pokeDetails.id})`}
-      />
-      <h1>
-        {generatePokeName(pokeDetails.name)}
-        <button
-          className="poke-details__play-button"
-          onClick={playCry}
-          disabled={cryPlaying}
-        >
-          <Icon
-            className="poke-details__play-button__icon"
-            icon={faPlayCircle}
-          />
-        </button>
-      </h1>
+      <button
+        className="poke-details__play-button"
+        onClick={playCry}
+        disabled={cryPlaying}
+      >
+        <img
+          className="poke-details__play-button__image"
+          src={pokeDetails.media.artwork}
+          alt={`official artwork for ${pokeDetails.name}(#${pokeDetails.id})`}
+        />
+      </button>
+      <h1>{generatePokeName(pokeDetails.name)}</h1>
 
       <p>
         <span className="poke-details__property-name">Pokédex Number: </span>
