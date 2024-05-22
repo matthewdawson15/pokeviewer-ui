571a0706118a38ecfce538d979888691bf5fa63d
Matthew Dawson <60929307+matthewdawson15@users.noreply.github.com>
2024-05-22 09:27:47 +0100

    change loading spinner to defined func

--- src/blocks/LoadingSpinner/LoadingSpinner.tsx
+++ src/blocks/LoadingSpinner/LoadingSpinner.tsx
@@ -13,25 +13,27 @@ interface LoadingSpinnerProps extends OptionalClassProps {
  * Block component to render a spinning loading spinner with
  * optional text and classname
  */
-const LoadingSpinner = ({
+function LoadingSpinner({
   className = "",
   height,
   text = "",
-}: LoadingSpinnerProps) => (
-  <div
-    className="loading-spinner-container"
-    style={height ? { height: `${height}px` } : {}}
-  >
-    <Icon
-      className={`loading-spinner-container__loading-spinner ${className}`}
-      icon={faSyncAlt}
-    />
-    {text && (
-      <span className="loading-spinner-container__loading-spinner-text">
-        {text}
-      </span>
-    )}
-  </div>
-);
+}: LoadingSpinnerProps) {
+  return (
+    <div
+      className="loading-spinner-container"
+      style={height ? { height: `${height}px` } : {}}
+    >
+      <Icon
+        className={`loading-spinner-container__loading-spinner ${className}`}
+        icon={faSyncAlt}
+      />
+      {text && (
+        <span className="loading-spinner-container__loading-spinner-text">
+          {text}
+        </span>
+      )}
+    </div>
+  );
+}
 
 export default LoadingSpinner;
