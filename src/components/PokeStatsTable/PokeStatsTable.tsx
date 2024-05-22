b95384916f7fd8eb7fd0f180425d11d2e4979452
Matthew Dawson <60929307+matthewdawson15@users.noreply.github.com>
2024-05-22 01:14:27 +0100

    added poke stats table component

---
+++ src/components/PokeStatsTable/PokeStatsTable.tsx
@@ -0,0 +1,32 @@
+import React, { ReactElement } from "react";
+import { StatDTO } from "../../types/pokemonDTO";
+import "./PokeStatsTable.scss";
+
+interface PokeStatsTableProps {
+  pokeStats: StatDTO[];
+}
+
+function PokeStatsTable({ pokeStats }: PokeStatsTableProps): ReactElement {
+  return (
+    <table className="poke-stats-table">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Base Stat</th>
+          <th>EV</th>
+        </tr>
+      </thead>
+      <tbody>
+        {pokeStats.map((stat, index) => (
+          <tr key={index}>
+            <td>{stat.name}</td>
+            <td>{stat.baseStat}</td>
+            <td>{stat.effort}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default PokeStatsTable;
